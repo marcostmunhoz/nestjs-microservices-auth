c1875265a909c09de8df79eaad2260a90157f215
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2023-05-03 17:43:17 -0300

    chore: setup nestjs project

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { protobufPackage } from './auth.pb';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<GrpcOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: 'localhost:50051',
+      package: protobufPackage,
+      protoPath: join(
+        'node_modules',
+        'nestjs-microservices-proto',
+        'proto',
+        'auth.proto',
+      ),
+    },
+  });
+
+  await app.listen();
+}
+bootstrap();
